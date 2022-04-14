f9f77372c932586859d399ed2549d982291f251c
b.stone <b.stone@2018.ljmu.ac.uk>
2022-04-14 17:11:59 +0000

    Added publisher and subscriber test code

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -1,4 +1,3 @@
-
 //Object data modelling library for mongo
 const mongoose = require('mongoose');
 
@@ -15,12 +14,20 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+//Get the hostname of the node
+var os = require("os");
+var myHostname = os.hostname();
+var amqp = require('amqplib/callback_api');
+
+//print the hostname
+console.log("------------ HOSTNAME [%s] ------------",myHostname);
+
 //connection string listing the mongo servers. This is an alternative to using a load balancer. THIS SHOULD BE DISCUSSED IN YOUR ASSIGNMENT.
 const connectionString = 'mongodb://localmongo1:27017,localmongo2:27017,localmongo3:27017/NotFLIX?replicaSet=rs0';
 
 setInterval(function() {
 
-  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
+	console.log(`Intervals are used to fire a function for the lifetime of an application.`);
 
 }, 3000);
 
@@ -36,34 +43,133 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var Schema = mongoose.Schema;
 
-var notFlixSchema = new Schema({
-  _id : Number,
-  accountID: Number,
-  userName: String,
-  titleID: Number,
-  userAction: String,
-  dateAndTime: Date,
-  pointOfInteraction: String,
-  typeOfInteraction: String
+var logsSchema = new Schema({
+	_id : Number,
+	accountID: Number,
+	username: String,
+	titleID: Number,
+	userAction: String,
+	dateAndTime: Date,
+	pointOfInteraction: String,
+	typeOfInteraction: String
+});
 
 var logsModel = mongoose.model('Logs', logsSchema, 'logs');
 
+//SUBSCRIBER CODE
+amqp.connect('amqp://test:test@172.16.70.30', function(error0, connection) {
+      if (error0) {
+              throw error0;
+            }
+      connection.createChannel(function(error1, channel) {
+              if (error1) {
+                        throw error1;
+                      }
+              var exchange = 'logs';
+
+              channel.assertExchange(exchange, 'fanout', {
+                        durable: false
+                      });
+
+              channel.assertQueue('', {
+                        exclusive: true
+                      }, function(error2, q) {
+                                if (error2) {
+                                            throw error2;
+                                          }
+                                console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+                                channel.bindQueue(q.queue, exchange, '');
+
+                                channel.consume(q.queue, function(msg) {
+                                            if(msg.content) {
+                                                            console.log(" [x] %s", msg.content.toString());
+                                                          }
+                                          }, {
+                                                      noAck: true
+                                                    });
+                              });
+            });
+});
+
+
+//PUBLISHER CODE
+amqp.connect('amqp://test:test@192.168.56.109', function(error0, connection) {
+      if (error0) {
+              throw error0;
+            }
+      connection.createChannel(function(error1, channel) {});
+});
+
+amqp.connect('amqp://test:test@172.16.70.30', function(error0, connection) {
+
+if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function(error1, channel) {
+              if (error1) {
+                        throw error1;
+                      }
+              var exchange = 'logs';
+              var msg =  'Hello World!';
+
+              channel.assertExchange(exchange, 'fanout', {
+                        durable: false
+                      });
+              channel.publish(exchange, '', Buffer.from(msg));
+              console.log(" [x] Sent %s", msg);
+            });
+
+
+
+
+
+    setTimeout(function() {
+              connection.close();
+              }, 500);
+});
+
+
+setInterval(function() {
+  console.log("============= LEADER CHECK [1] =============");
+	console.log(JSON.stringify(nodes));
+	leader = 1;
+	activeNodes = 0;
+	Object.entries(nodes).forEach(([hostname,prop]) => {
+	  console.log("test: " + JSON.stringify(hostname) + JSON.stringify(prop) )
+	  maxNodeID = myNodeID;
+	  if(hostname != myHostname){
+	    if("nodeID" in prop){
+			  activeNodes++;
+        //console.log("ACTIVE NODES: " + activeNodes + "LENGTH: " + nodes.length);
+   		  if(prop.nodeID > myNodeID){
+			    leader = 0;
+			  }
+	    }
+	  }
+		if((leader == 1) && (activeNodes == nodes.length)){
+		  systemLeader = 1;
+		  console.log(myHostname + ": I am the leader now! NODE: " + myNodeID);
+      myLeaderNode = myNodeID;
+      //nodes.find(e => e.nodeID === myNodeID).leaderNode = myNodeID;
+		}
+  });
+}, 2000);
 
 
 app.get('/', (req, res) => {
-     logsModel.find({},'_id accountID userName titleID userAction dateAndTime pointOfInteraction typeOfInteraction lastName', (err, logs) => {
-	if(err) return handleError(err);
-    res.send(JSON.stringify(stock))
-  }) 
+     logsModel.find({},'_id accountID username titleID userAction dateAndTime pointOfInteraction typeOfInteraction', (err, logs) => {
+        if(err) return handleError(err);
+    res.send(JSON.stringify(logs))
+  })
 })
 
-app.post('/',  (req, res) => { 
-  var new_logs_instance = new logsModel(req.body); 
-  new_logs_instance.save(function (err) { 
-  if (err) res.send('Error'); 
-    res.send(JSON.stringify(req.body)) 
-  }); 
-}) 
+app.post('/',  (req, res) => {
+  var new_logs_instance = new SomeModel(req.body);
+  new_logs_instance.save(function (err) {
+  if (err) res.send('Error');
+    res.send(JSON.stringify(req.body))
+  });
+})
 
 app.put('/',  (req, res) => {
   res.send('Got a PUT request at /')
